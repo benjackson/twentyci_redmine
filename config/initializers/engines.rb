f02d2598945d9ab2d3642e075c98b889c7793966
Ben Jackson <ben@lissomsolutions.com>
2011-08-11 11:24:22 +0100

    Wrong dir

--- config/initializers/engines.rb
+++ config/initializers/engines.rb
@@ -1,1 +1,1 @@
-Engines.public_directory = File.join(File.dirname(__FILE__), "..", "tmp", "plugin_assets") if RAILS_ENV == "production"
+Engines.public_directory = File.join(File.dirname(__FILE__), "..", "..", "tmp", "plugin_assets") if RAILS_ENV == "production"
